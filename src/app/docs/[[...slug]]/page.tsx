ad14055164242c1dcc70efc21e7148f9ebe9f978
Rodney Osodo <socials@rodneyosodo.com>
2026-02-06 11:57:33 +0300

    fix(docs): update GitHub URL to absmach/propeller-website
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -26,7 +26,7 @@ export default async function Page(props: PageProps<"/docs/[[...slug]]">) {
         <LLMCopyButton markdownUrl={`${page.url}.mdx`} />
         <ViewOptions
           markdownUrl={`${page.url}.mdx`}
-          githubUrl={`https://github.com/rodneyosodo/special-giggle/blob/main/content/docs/${page.path}`}
+          githubUrl={`https://github.com/absmach/propeller-website/blob/main/content/docs/${page.path}`}
         />
       </div>
 
